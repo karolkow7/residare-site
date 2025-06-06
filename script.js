78ffd79376d36d4fa720ddc746a9614963715571
karolkow7 <karol_kow@yahoo.de>
2025-06-06 21:26:11 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-// Parallax-Effekt auf Text
-window.addEventListener("scroll", function () {
-  const text = document.querySelector(".hero-content");
-  const offset = window.pageYOffset;
-  text.style.transform = `translateY(${offset * 0.4}px)`;
-});
